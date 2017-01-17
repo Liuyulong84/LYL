8229fe6653caa041ed04fac4b43c17610f785697
liuyulong <liuyulong@rd.tuan800.com>
2017-01-17 18:08:43 +0800

    test

--- app/src/main/java/com/lyl/MainActivity.java
+++ app/src/main/java/com/lyl/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.administrator.lyl;
+package com.lyl;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.lyl.R;
 
 public class MainActivity extends AppCompatActivity {
 
